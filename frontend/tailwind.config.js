f49b0234f7a44095dfc75ca90332c693c62f6834
Devin Hua <devinhua004@gmail.com>
2026-03-04 17:46:16 -0800

    Draft website to show business owners

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,9 +2,19 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{html,js}",
+    "./src/**/*.{html,js, jsx}",
     "./node_modules/tw-elements/js/**/*.js",
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        heading: ['"Delius Unicase"', 'cursive'], 
+        body: ['ui-sans-serif', 'system-ui'],     
+        pinyon: ['"Pinyon Script"', 'cursive'],  
+        explora: ['"Explora Regular"', 'serif'], 
+      },
+    },
+  },
   plugins: [require("tw-elements/plugin.cjs")],
   darkMode: "class"
 };
