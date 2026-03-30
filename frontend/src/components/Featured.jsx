0a67ba1933df55331520975e640b87f71900125a
Devin Hua <devinhua004@gmail.com>
2026-03-29 18:05:45 -0700

    Updated featured section. Fixed some menu items

--- frontend/src/components/Featured.jsx
+++ frontend/src/components/Featured.jsx
@@ -5,33 +5,48 @@ export default function Featured() {
     <section id="featured" className="px-8 py-16 bg-white space-y-8 text-[#333333]">
       <div className="text-center mb-8">
         <p className="text-[#D4AF37] font-semibold tracking-widest uppercase text-sm mb-2">
-          Fan Favorites
+          Coming Soon!
         </p>
         <h3 className="text-gray-800 text-3xl md:text-4xl font-bold">
-          Featured Desserts
+          New Drinks!
         </h3>
       </div>
 
       <FeaturedItem
-        title="Nutella Bear"
-        description="A decadent, airy Nutella mousse shaped like a bear, with a silky smooth texture and a rich chocolatey flavor that melts in your mouth. Sweet, indulgent, and impossible to resist!"
-        image="/bear-removebg-preview.png"
-        hoverColor="hover:bg-yellow-800/30"
+        title="Jasmine Green Tea"
+        description=""
+        image=""
+        hoverColor="hover:bg-green-600/70"
       />
 
       <FeaturedItem
-        title="Ube Cheesecake"
-        description="Creamy cheesecake infused with sweet ube for a silky, lightly nutty dessert with a signature purple glow."
-        image="/ube_cheesecake-removebg-preview.png"
+        title="Taro"
+        description=""
+        image=""
         reverse
         hoverColor="hover:bg-purple-800/30"
       />
 
       <FeaturedItem
-        title="Strawberry Mousse"
-        description="A fluffy, creamy strawberry dessert bursting with sweet berry flavor."
-        image="/strawberry_mousse-removebg-preview.png"
-        hoverColor="hover:bg-red-800/50"
+        title="Hokkaido"
+        description=""
+        image=""
+        hoverColor="hover:bg-blue-800/50"
+      />
+
+      <FeaturedItem
+        title="Mango"
+        description=""
+        image=""
+        reverse
+        hoverColor="hover:bg-orange-600/70"
+      />
+
+      <FeaturedItem
+        title="Brown Sugar Okinawa"
+        description=""
+        image=""
+        hoverColor="hover:bg-amber-900/70"
       />
     </section>
   );
--- frontend/src/components/FeaturedItem.jsx
+++ frontend/src/components/FeaturedItem.jsx
@@ -5,22 +5,28 @@ export default function FeaturedItem({ title, description, image, reverse = fals
     >
       {/* Image */}
       <div
-        className={`md:w-1/2 relative flex items-center justify-center ${
-          reverse ? "md:order-2" : ""
-        }`}
+        className={`md:w-1/2 relative flex items-center justify-center ${reverse ? "md:order-2" : ""
+          }`}
       >
-        <img
+        {/* <img
           src={image}
           alt={title}
           className="w-3/4 h-auto object-contain rounded-2xl -mt-20"
-        />
+        /> */}
+
+        {/* Placeholder Image */}
+        <div className="w-full h-48 bg-neutral-100 rounded-2xl flex flex-col items-center justify-center text-gray-500">
+          <span className="text-2xl">🧋</span>
+          <p className="text-sm mt-2">Photo Coming Soon</p>
+        </div>
+
+
       </div>
 
       {/* Text */}
       <div
-        className={`md:w-1/2 flex flex-col justify-center ${
-          reverse ? "md:order-1" : ""
-        }`}
+        className={`md:w-1/2 flex flex-col justify-center ${reverse ? "md:order-1" : ""
+          }`}
       >
         <h2 className="text-5xl font-bold mb-4">{title}</h2>
         <p>{description}</p>
